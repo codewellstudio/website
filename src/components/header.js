8da58b5d7699b91f14cbdefe34ee76681bb9d905
Sinisa Mikulic <sinisa.mikulic@gmail.com>
2019-11-02 22:35:30 +0100

    Add scrolling and text animation for section content

--- src/components/header.js
+++ src/components/header.js
@@ -1,32 +1,83 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import scrollToElement from "scroll-to-element"
+
+import Logo from "./logo"
+
+const handleAnchorLink = (hash) => {
+  scrollToElement(hash, {
+    align: 'top',
+    duration: 1000,
+  })
+}
 
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
+      zIndex: 1,
+      position: `fixed`,
+      top: `0`,
+      width: `100%`,
+      height: `60px`,
+      background: `white`,
+      fontFamily: `-apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif`,
+      boxShadow: `0 -4px 6px #000`,
     }}
   >
+    {/* <div style={{ maxWidth: `200px` }}>
+      <Logo />
+    </div> */}
     <div
       style={{
         margin: `0 auto`,
         maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
+        height: `60px`,
+        display: `flex`,
+        alignItems: `center`,
+        justifyContent: `space-between`,
+        color: `rgb(18, 36, 49)`,
+        fontSize: `0.85028rem`, // h5
+        fontWeight: 200,
+        // padding: `1rem`,
       }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+      <span
+        onClick={() => handleAnchorLink('#what-we-do')}
+        style={{
+          textDecoration: `none`,
+          cursor: `pointer`,
+        }}
+      >
+        What we do
+      </span>
+      <span
+        onClick={() => handleAnchorLink('#how-we-do-it')}
+        style={{
+          textDecoration: `none`,
+          cursor: `pointer`,
+        }}
+      >
+        How we do it
+      </span>
+      <span
+        onClick={() => handleAnchorLink('#section3')}
+        style={{
+          textDecoration: `none`,
+          cursor: `pointer`,
+        }}
+      >
+        Section 3
+      </span>
+      <span
+        onClick={() => handleAnchorLink('#section4')}
+        style={{
+          textDecoration: `none`,
+          cursor: `pointer`,
+        }}
+      >
+        Section 4
+      </span>
     </div>
   </header>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,65 @@
 import React from "react"
+import Fade from 'react-reveal/Fade';
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+import "./index.css"
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Code well studio" />
+    <section id="welcome">
+      <Fade bottom cascade>
+        <div>
+          <h1>Building successful and profitable products is difficult. We make it simple.</h1>
+          <h3>Whether you're a Fortune 500 or a startup in stealth mode - we provide our clients the creative, technical and business expertise they need to succeed.</h3>
+        </div>
+      </Fade>
+    </section>
+    <section id="what-we-do">
+      <Fade bottom cascade>
+        <div className="subsection-left">
+          <h1>What we do</h1>
+          <div className="services">
+            <h3>› deliver measurable results by showcasing working and improved product</h3>
+            <h3>› ensure effective and transparent communication that keeps everyone on the same page</h3>
+            <h3>› focus on achieving client’s business goals and bringing value</h3>
+          </div>
+        </div>
+      </Fade>
+    </section>
+    <section id="how-we-do-it">
+      <Fade bottom cascade>
+        <div className="subsection-right">
+          <h1>How we do it?</h1>
+          <h3>
+            Most projects end up with similar problems. 
+            Application <b>performance</b>, development <b>process</b>, product <b>delivery speed</b> and customer <b>happiness</b>. 
+            Our technical solution always provides the best balance of those key ingredients and maximizes your chance of success.
+          </h3>
+          <div className="subsection-left top-spacing">
+            <h2>We cover a wide range of skills</h2>
+            <p>
+              We are experts in user experience development, performance optimization, and frontend architecture design. 
+              Besides our speciality, we can also help you with user research, product development, development process optimization, and much more.
+            </p>
+          </div>
+        </div>
+      </Fade>
+    </section>
+    <section id="section3">
+      <Fade bottom>
+        <h1>Hi, I'm Jane Doe</h1>
+        <h3>I'm creating noice web experiences for the next generation of consumer-facing companies</h3>
+      </Fade>
+    </section>
+    <section id="section4">
+      <Fade bottom>
+        <h1>Hi, I'm Jane Doe</h1>
+        <h3>I'm creating noice web experiences for the next generation of consumer-facing companies</h3>
+      </Fade>
+    </section>
   </Layout>
 )
 
