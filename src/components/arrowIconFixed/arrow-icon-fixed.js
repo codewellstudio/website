8de3edfa5c0819fab68ff8db1b73c76f064c2b34
Sinisa Mikulic <sinisa.mikulic@gmail.com>
2019-11-08 02:52:49 +0100

    Update window check fix

--- src/components/arrowIconFixed/arrow-icon-fixed.js
+++ src/components/arrowIconFixed/arrow-icon-fixed.js
@@ -6,12 +6,12 @@ import "./arrow-icon-fixed.css"
 
 const ArrowIconFixed = ({ anchors }) => {
   const [lastSection, onLastSectionMark] = useState(false);
-  const windowInnerHeight = window !== undefined ? window.innerHeight : 0;
+  const windowInnerHeight = typeof window !== 'undefined' ? window.innerHeight : 0;
   const lastSectionAnchor = (anchors.length - 2) * windowInnerHeight;
 
   const onScrollDown = ({ anchors }) => {
-    const windowInnerHeight = window !== undefined ? window.innerHeight : 0;
-    const windowScrollY = window !== undefined ? window.scrollY : 0;
+    const windowInnerHeight = typeof window !== 'undefined' ? window.innerHeight : 0;
+    const windowScrollY = typeof window !== 'undefined' ? window.scrollY : 0;
 
     if (windowScrollY > lastSectionAnchor) {
       onLastSectionMark(true)
